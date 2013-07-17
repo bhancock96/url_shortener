54e78c287c479434eb0fab77b53efba92388ca7a
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 08:42:51 -0500

    Update controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,13 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/urls' do
+  url = params[:url]
+  Url.new(full_url: url, short_url: 'DO SOMETHING', click_count: 0)
+  # full_url: string, short_url: string, counter: integer
+end
+
+get '/:short_url' do
+
+end
